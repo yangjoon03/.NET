0712d7a249050bb072be3b9d023231a2bda2ba96
yangjunyeong <pythoncloud0115@gmail.com>
2025-08-09 19:19:49 +0900

    텍스트 실시간 바인딩

---
+++ MS-Chapter/Chapter3/MsChapter3Binding.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui_study
+{
+
+    public class MsChapter3Binding : INotifyPropertyChanged
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
